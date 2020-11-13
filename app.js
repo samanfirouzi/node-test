d51a2cc1561537ded64614f7505ecf78482cdb4b
saman firouzi <saman.firouzi@gmail.com>
2020-11-12 23:17:05 -0500

    change

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
 
 const port = process.env.port || 3000;
 const wss = new WebSocket.Server({ port: port });
 
+let cl = new Map();
+let clr = new Map();
 wss.on('connection', function connection(ws) {
+    console.log('new connection');
+    // uuid = uuidv4();
+    // console.log(uuid);
+    // cl[ws] = uuid;
+    // clr[uuid] = ws;
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
+
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
     });
 
     ws.send('something from server');
+
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+});
+
+function noop() { }
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+
+        ws.isAlive = false;
+        ws.ping(noop);
+    });
+}, 30000);
+
+wss.on('close', function close() {
+    clearInterval(interval);
 });
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://nodeawstest-env.eba-vjg2qn3w.us-east-2.elasticbeanstalk.com/');
+// const ws = new WebSocket('ws://nodeawstest-env.eba-vjg2qn3w.us-east-2.elasticbeanstalk.com/');
+const ws = new WebSocket('ws://127.0.0.1:3000');
+function heartbeat() {
+    clearTimeout(this.pingTimeout);
+
+    // Use `WebSocket#terminate()`, which immediately destroys the connection,
+    // instead of `WebSocket#close()`, which waits for the close timer.
+    // Delay should be equal to the interval at which your server
+    // sends out pings plus a conservative assumption of the latency.
+    this.pingTimeout = setTimeout(() => {
+        this.terminate();
+    }, 30000 + 1000);
+}
 
 ws.on('open', function open() {
     ws.send('something from client');
@@ -8,4 +20,10 @@ ws.on('open', function open() {
 
 ws.on('message', function incoming(data) {
     console.log(data);
+});
+
+
+ws.on('ping', heartbeat);
+ws.on('close', function clear() {
+    clearTimeout(this.pingTimeout);
 });
